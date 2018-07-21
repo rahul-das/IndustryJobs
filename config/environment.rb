69eb75265577bcc1aa02d6ed58006ea05102c213
Rahul Kumar Das <rahul.das@tiatros.com>
2018-07-22 01:31:37 +0530

    small changes

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :password => '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',
   :domain => 'fast-depths-56174.herokuapp.com',
   :address => 'smtp.sendgrid.net',
-  :port => 465,
+  :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
