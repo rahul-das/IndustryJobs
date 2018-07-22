a911ee3ebc20037511dee08f987eeb0c0632d581
Rahul Kumar Das <rahul.das@tiatros.com>
2018-07-23 00:10:06 +0530

    ENV['SENDGRID_API_KEY']

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
-  :password => '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',
+  :password => ENV['SENDGRID_API_KEY'],
   :domain => 'fast-depths-56174.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
