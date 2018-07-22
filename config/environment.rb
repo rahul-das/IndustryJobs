c3f9b137fcd0a94c1eac024e0428001d96314252
Rahul Kumar Das <rahul.k.das@outlook.com>
2018-07-22 23:45:04 +0530

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
-  :password => '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',
+  :password => '',
   :domain => 'fast-depths-56174.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
